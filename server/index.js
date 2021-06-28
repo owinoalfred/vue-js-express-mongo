aee00304efee581cea5a29993b0a3d8f054e6be7
alfred <59254825+owinoalfred@users.noreply.github.com>
2021-06-28 13:24:56 +0300

    adding post.js

--- server/index.js
+++ server/index.js
@@ -6,9 +6,14 @@ const app = express();
 
 //middle ware
 app.use(bodyParser.json());
-app.use(cors);
+app.use(cors());
+
+const posts = require('./routes/api/posts');
+
+app.use("/api/posts", posts);
 
 
 const port = process.env.PORT || 5000;
+
 
 app.listen(port, () => console.log(`Server started on port ${port}` ));
---
+++ server/routes/api/post.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongodb = require('mongodb')
+
+const router = express.Router()
+
+ 
+//get post
+router.get('/', (req, res) =>{
+    res.send("hello")
+})
+
+//Add post
+
+
+
+
+//delete post
+
+
+module.exports = router;
