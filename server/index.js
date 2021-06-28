63fe3037555c0faae8fda28975614fcc3e186bd6
alfred <59254825+owinoalfred@users.noreply.github.com>
2021-06-28 12:55:31 +0300

    creating the routes

--- server/index.js
+++ server/index.js
@@ -11,4 +11,4 @@ app.use(cors);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port,()=> console.log(`Server started on port ${port}` ));
+app.listen(port, () => console.log(`Server started on port ${port}` ));
